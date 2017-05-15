58537c61f55e57fffb55334f58b89b958761810f
Montana Sawyer <montanarsawyer@gmail.com>
2017-05-15 16:39:53 -0700

    add mui to EventCard

--- imports/ui/containers/Browse/Browse.js
+++ imports/ui/containers/Browse/Browse.js
@@ -1,35 +1,17 @@
 import React, { Component } from 'react';
-import FloatingActionButton from 'material-ui/FloatingActionButton';
 import IconButton from 'material-ui/IconButton';
-import ContentCreate from 'material-ui/svg-icons/content/create';
-import SocialPerson from 'material-ui/svg-icons/social/person';
-import BulletedList from 'material-ui/svg-icons/editor/format-list-bulleted';
 import ActionFavorite from 'material-ui/svg-icons/action/favorite';
 import ContentClear from 'material-ui/svg-icons/content/clear';
-import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+
+import EventCard from '../../components/EventCard';
 
 class Browse extends Component {
   render() {
     return (
       <div>
         <IconButton><ContentClear /></IconButton>
-          <Card>
-            <CardMedia
-              overlay={<CardTitle title="Go To The Park" subtitle="with Susan" />}
-            >
-            </CardMedia>
-            <CardTitle title="11:00am" subtitle="When" />
-            <CardTitle title="City Park" subtitle="Where" />
-            <CardText>
-              Bring your sweaters and a smile
-            </CardText>
-          </Card>
+          <EventCard />
         <IconButton><ActionFavorite /></IconButton>
-        <div>
-          <FloatingActionButton><SocialPerson /></FloatingActionButton>
-          <FloatingActionButton><ContentCreate /></FloatingActionButton>
-          <FloatingActionButton><BulletedList /></FloatingActionButton>
-        </div>
       </div>
     );
   }
--- imports/ui/containers/NotFound/NotFound.js
+++ imports/ui/containers/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import AlertError from 'material-ui/svg-icons/alert/error';
 
 class NotFound extends Component {
   render() {
     return (
       <div>
+        <AlertError />
         <h1> I am sorry but that doesn't exist </h1>
       </div>
     );
