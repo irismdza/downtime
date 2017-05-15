d1f43a9e1c9bc9bf146761584b17331c430b264c
Iris <mdza.iris@gmail.com>
2017-05-15 16:26:19 -0700

    add route for sort events container

--- client/main.js
+++ client/main.js
@@ -14,10 +14,8 @@ import { browserHistory } from 'react-router';
 
 // route components
 import App from '../imports/ui/containers/App';
-import EventsAttending from '../imports/ui/containers/EventsAttending';
-import EventsHosting from '../imports/ui/containers/EventsHosting';
 import EventPage from '../imports/ui/containers/EventPage';
-import EventsList from '../imports/ui/containers/EventsList';
+import SortEventsContainer from '../imports/ui/containers/SortEventsContainer';
 import Browse from '../imports/ui/containers/Browse';
 import CreateEvent from '../imports/ui/containers/CreateEvent';
 import NotFound from '../imports/ui/containers/NotFound';
@@ -32,9 +30,7 @@ Meteor.startup(() => {
             <Switch>
               <Route exact path="/" component={Browse} />
               <Route exact path="/create-event" component={CreateEvent} />
-              <Route exact path="/events" component={EventsList} />
-              <Route exact path="/events/attending" component={EventsAttending} />
-              <Route exact path="/events/hosting" component={EventsHosting} />
+              <Route exact path="/events" component={SortEventsContainer} />
               <Route exact path="/events/:event_id/event" component={EventPage} />
               <Route path="*" component={NotFound} />
               {/*<Route exact path="profile/:user_id" component={UserProfile} />*/}
