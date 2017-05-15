7c2699a10c91657107059d55335dbf5dba554291
Iris <mdza.iris@gmail.com>
2017-05-15 14:52:12 -0700

    add buttons to events list

--- imports/ui/containers/EventsList/EventsList.js
+++ imports/ui/containers/EventsList/EventsList.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
 
 class EventsList extends Component {
   render() {
     return (
       <div>
-        EVENTS LIST CONTAINER
+        <h1>upcoming events</h1>
+        <RaisedButton label="events i'm hosting" primary={true} />
+        <br/>
+        <RaisedButton label="events i'm attending" primary={true} />
       </div>
     );
   }
