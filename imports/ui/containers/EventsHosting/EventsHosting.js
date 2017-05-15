e364a4a4d2fc9756d3a98dd164639271e48c3224
Iris <mdza.iris@gmail.com>
2017-05-15 15:22:50 -0700

    add list to events hosting page

--- imports/ui/containers/EventsHosting/EventsHosting.js
+++ imports/ui/containers/EventsHosting/EventsHosting.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import Subheader from 'material-ui/Subheader';
+import {List, ListItem} from 'material-ui/List';
+import Divider from 'material-ui/Divider';
 
 class EventsHosting extends Component {
   render() {
     return (
       <div>
-        EVENTS HOSTING CONTAINER
+        <List>
+        <Subheader>EVENTS I'M HOSTING TODAY</Subheader>
+        <ListItem
+          primaryText="brunch?"
+          secondaryText={
+            <div>
+              <p>
+                11:00 AM / Twisted fork on Granville
+              </p>
+            </div>
+          }
+          secondaryTextLines={2}
+        />
+        <Divider inset={true} />
+        <ListItem
+          primaryText="Launch open mic!"
+          secondaryText={
+            <div>
+              <p>
+                7:30 AM / Roundhouse Community Centre in Yaletown
+              </p>
+            </div>
+          }
+          secondaryTextLines={2}
+        />
+        <Divider inset={true} />
+        </List>
       </div>
     );
   }
