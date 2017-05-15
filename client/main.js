134f9888ba358acf3210af0cfd94557ce363c033
Montana Sawyer <montanarsawyer@gmail.com>
2017-05-15 09:47:59 -0700

    build file structure

--- client/main.js
+++ client/main.js
@@ -2,11 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
-const App = () => (
-  <MuiThemeProvider>
-  </MuiThemeProvider>
-);
+import { App } from '../imports/ui/containers/App/';
 
 Meteor.startup(() => {
   Meteor.autorun(() => {
---
+++ imports/ui/containers/App/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { createContainer } from 'meteor/react-meteor-data';
+
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider>
+      </MuiThemeProvider>
+    )
+  }
+}
+
+export default App;
