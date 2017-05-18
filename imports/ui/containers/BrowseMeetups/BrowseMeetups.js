31f21694f3a63f6582d277d0f0f60018dafc2126
Iris <mdza.iris@gmail.com>
2017-05-18 11:43:26 -0700

    rename browse to browseMeetups

--- imports/ui/containers/BrowseMeetups/BrowseMeetups.js
+++ imports/ui/containers/BrowseMeetups/BrowseMeetups.js
@@ -4,20 +4,20 @@ import ActionFavorite from 'material-ui/svg-icons/action/favorite';
 import ContentClear from 'material-ui/svg-icons/content/clear';
 import { createContainer } from 'meteor/react-meteor-data';
 
-import EventCard from '../../components/EventCard';
+import MeetupInfoCard from '../../components/MeetupInfoCard';
 
 
-class Browse extends Component {
+class BrowseMeetups extends Component {
   render() {
     return (
       <div className='browse-container'>
         <IconButton className='browse-icon-button'><ContentClear /></IconButton>
-          <div className='browse-event-card'><EventCard /></div>
+          <div className='browse-meetup-card'><MeetupInfoCard /></div>
         <IconButton className='browse-icon-button'><ActionFavorite /></IconButton>
       </div>
     );
   }
 }
 
-export default Browse;
+export default BrowseMeetups;
 
